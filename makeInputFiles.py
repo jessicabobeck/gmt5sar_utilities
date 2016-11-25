6babbb286c116576a6fc27b49421ce26ee0f1b41
Jessica <jessica.bobeck@colorado.edu>
2016-11-25 00:46:40 -0700

    Baseline Plot complete, leapfrog function working, still working on plot

--- makeInputFiles.py
+++ makeInputFiles.py
@@ -41,148 +41,200 @@ import matplotlib.colors as colors
 import numpy as np
 
 
-def get_cmap(N):
-    '''Returns a function that maps each index in 0, 1, ... N-1 to a distinct 
-    RGB color.'''
-    color_norm  = colors.Normalize(vmin=0, vmax=N-1)
-    scalar_map = cmx.ScalarMappable(norm=color_norm, cmap='hsv') 
-    def map_index_to_rgb_color(index):
-        return scalar_map.to_rgba(index)
-    return map_index_to_rgb_color
 
-
-
-
-table = 'baseline_table.dat'
+table = 'baseline_table_thesis.dat'
 master = 'IMG-HH-ALPSRP052541220-H1.0__A'
 sat = 'ALOS'
-algorithm = 'baseline'
-prim_base = 10000
+algorithm = 'leapfrog'
+prim_base = 1000
+prim_year =2
+sec_base = 1500
+sec_year = 1
+ter_base = 500
+ter_year = 2
 
-def makeInputFiles(table, master, sat, algorithm, prim_base=800, prim_year=2,
-                   sec_base=1500, sec_year=1, ter_base=500, ter_year=2):
+#def makeInputFiles(table, master, sat, algorithm, prim_base=800, prim_year=2,
+     #              sec_base=1500, sec_year=1, ter_base=500, ter_year=2):
 
     #open table
-    with open(table) as f:
-        lines = f.readlines()
-    ids = []
-    date = []
-    base = []
+with open(table) as f:
+    lines = f.readlines()
+ids = []
+date = []
+base = []
+ticks = []
 
-    
-    #prepare data
-    for i in range(len(lines)):
-        new = lines[i].split()
-        ids.append(new[0])
-        date.append(int(new[1][0:7]))
-        base.append(float(new[4]))
-    
-    data = zip(date,base,ids)
 
-    #sort by baseline
-    data.sort(key=lambda a:a[1])
+#prepare data
+for i in range(len(lines)):
+    new = lines[i].split()
+    ids.append(new[0])
+    date.append(new[1][0:7])
+    ticks.append(new[1][0:4])
+    base.append(float(new[4]))
+
+data = zip(date,base,ids)
+
+#sort by baseline
+data.sort(key=lambda a:a[1])
+
+#--------------------------Algorithm---------------------------------------------
+
+#BASELINE---------------------------------
+#if algorithm == 'baseline':
+#    groups = [[]]
+#    last_row = None
+#    for row in data:
+#        #sort into groups by threshold
+#        if last_row is not None and row[1] - last_row[1] > prim_base:
+#            groups.append([])
+#        last_row = row
+#        groups[-1].append(row)
+#        
+#
+#comb = []
+#for j in range(len(groups)):
+#    comb.append(list(set(it.combinations(groups[j], 2))))
+#    
+##plot
+#    
+#rc('xtick', labelsize=20) 
+#rc('ytick', labelsize=20)
+#rc('lines', linewidth=3)
+#fig1 = plt.figure(figsize=(22,18))
+#ax = fig1.add_subplot(111)
+#ax.get_xaxis().get_major_formatter().set_useOffset(False)
+#ax.scatter(date,base,s=200)
+#ax.set_xlabel('Year',fontsize=25)
+#ax.set_ylabel('Baseline (m)',fontsize=25)
+# ax.set_title('Baseline Method')
+#
+#labels = []
+#for i in range(len(comb)):
+#    for j in range(len(comb[i])):
+#        x = []
+#        y = []
+#        x.append(comb[i][j][0][0])
+#        x.append(comb[i][j][1][0])
+#        
+#        y.append(comb[i][j][0][1])
+#        y.append(comb[i][j][1][1])
+#        
+#        if comb[i][j][0][2] not in labels:
+#            plt.annotate(comb[i][j][0][2], (x[0], y[0]),fontsize=16,horizontalalignment='right', verticalalignment='bottom')
+#
+#        labels.append(comb[i][j][0][2])
+#            
+#        if comb[i][j][1][2] not in labels:
+#            plt.annotate(comb[i][j][1][2], (x[1], y[1]),fontsize=16,horizontalalignment='left', verticalalignment='top') 
+#        
+#        labels.append(comb[i][j][1][2])
+#        
+#        ax.plot(x,y, c=np.random.rand(3,1))   
+#        
+#
+#plt.savefig('baseline_alignPairs.png', dpi=300)
+              
+#    #LEAPFROG--------------------------------
+if algorithm == 'leapfrog':
     
-    #--------------------------Algorithm---------------------------------------------
     
-    #BASELINE---------------------------------
-    if algorithm == 'baseline':
-        groups = [[]]
-        last_row = None
-        for row in data:
-            #sort into groups by threshold
-            if last_row is not None and row[1] - last_row[1] > prim_base:
-                groups.append([])
-            last_row = row
-            groups[-1].append(row)
+    #primary
+    primary = []
+    last_row = None
+    for row in data:
+        #sort primary pairs into groups by threshold distance and year
+        if last_row is not None and abs(row[1]) < prim_base:
+            if abs(int(row[0][0:4]) - int(last_row[0][0:4])) < prim_year and row[1] != 0:
+                primary.append(row)  
+        last_row = row
+    
+    #secondary        
+    secondary = []
+    last_row = None
+    if len(primary) == 0:
+        sys.exit('makeInputFiles Error: There is only 1 primary image (supermaster). Adjust thresholds.')
+    else:
+        for img in primary:
+            temp = []
+            for row in data:
+                #sort secondary pairs into groups by threshold distance and year   
+                if last_row is not None and abs(row[1]) < sec_base + img[1]:
+                    if abs(int(row[0][0:4]) - int(last_row[0][0:4])) < sec_year and row[1] != img[1] and row[1] != 0:
+                        temp.append(row) 
+                last_row = row
+            secondary.append(temp)
+                    
+    #tertiary
+    tertiary = []
+    last_row = None
+    if len(secondary) == 0:
+        sys.exit('makeInputFiles Error: There is only 1 secondary image (submaster). Adjust thresholds.')
+    else:
+        for group in secondary:
+            temp = []
+            for img in range(len(group)):
+                for row in data:
+                    #sort secondary pairs into groups by threshold distance and year
+                    if last_row is not None and abs(row[1]) > ter_base + group[img][1] and row[1] != group[img][0] and row[1] != 0:
+                        if abs(int(row[0][0:4]) - int(last_row[0][0:4])) > ter_year:
+                            temp.append(row)
+                    last_row = row
+                tertiary.append(temp)
+                
+#    groups = []
+#    groups.append(primary,secondary,tertiary)
+
+    mx = []
+    my = [] 
+    for row in data:
+        if row[1] == 0:
+            mx.append(row[0])
+            my.append(row[1])
+        
+    
+    px = []
+    py = []
+    for data in primary:
+        px.append(data[0])
+        py.append(data[1])
+        
+    sx = []
+    sy = []
+    for data in secondary:
+        for row in range(len(data)):
+            sx.append(data[row][0])
+            sy.append(data[row][1])
+    
+    tx = []
+    ty = []
+    for data in tertiary:
+        for row in range(len(data)):
+            tx.append(data[row][0])
+            ty.append(data[row][1])
             
-
-    comb = []
-    for j in range(len(groups)):
-        comb.append(list(set(it.combinations(groups[j], 2))))
         
-    #plot
-        
+#            
+#    ##plot
     rc('xtick', labelsize=20) 
     rc('ytick', labelsize=20)
     rc('lines', linewidth=3)
     fig1 = plt.figure(figsize=(22,18))
     ax = fig1.add_subplot(111)
+    plt.setp(ax, xticks=)
     ax.get_xaxis().get_major_formatter().set_useOffset(False)
-    ax.scatter(date,base,s=200)
     ax.set_xlabel('Year',fontsize=25)
     ax.set_ylabel('Baseline (m)',fontsize=25)
+    ax.set_title('Leapfrog Method', fontsize=32)
+      
+    ax.scatter(tx,ty, s=150, c='g')
+    ax.scatter(sx,sy,s=150, c='y')
+    ax.scatter(px,py,s=150, c='r')
+    ax.scatter(mx,my,s=150, c='b', marker='*')
+    
     
 
-    for i in range(len(comb)):
-        for j in range(len(comb[i])):
-            x = []
-            y = []
-            x.append(comb[i][j][0][0])
-            x.append(comb[i][j][1][0])
-            
-            y.append(comb[i][j][0][1])
-            y.append(comb[i][j][1][1])
-            
-            plt.annotate(comb[i][j][0][2], (x[0], y[0]),fontsize=16,horizontalalignment='right', verticalalignment='bottom')
-            plt.annotate(comb[i][j][1][2], (x[1], y[1]),fontsize=16,horizontalalignment='left', verticalalignment='top')
-            
-            ax.plot(x,y, c=np.random.rand(3,1))
 
-    plt.savefig('baseline_alignPairs.png', dpi=300)
-            
-            
-        
-        
-        
-        
-        
-        
-#        ax.scatter(date[i],base[i])
-#        ax.plot(data[i],base[i])
-#        plt.annotate(ids[i],(date[i],base[i]))
-
-        
-
-    
-#    
-#    #LEAPFROG--------------------------------
-#    elif algorithm == 'leapfrog':
-#        #primary
-#        primary = []
-#        last_row = None
-#        for row in data:
-#            #sort primary pairs into groups by threshold distance and year
-#            if last_row is not None and abs(row[2]) < prim_base:
-#                if abs(int(row[1][0:4]) - int(last_row[1][0:4])) < prim_year and row[2] != 0:
-#                    primary.append(row)  
-#            last_row = row
-#        
-#        #secondary        
-#        secondary = []
-#        last_row = None
-#        if len(primary) == 0:
-#            sys.exit('makeInputFiles Error: There is only 1 primary image (supermaster). Adjust thresholds.')
-#        else:
-#            for row in primary:
-#                #sort secondary pairs into groups by threshold distance and year   
-#                if last_row is not None and abs(row[2]) < sec_base:
-#                    if abs(int(row[1][0:4]) - int(last_row[1][0:4])) < sec_year and row[2] != 0:
-#                        secondary.append(row) 
-#                        
-#        #tertiary
-#        tertiary = []
-#        last_row = None
-#        if len(secondary) == 0:
-#            sys.exit('makeInputFiles Error: There is only 1 secondary image (submaster). Adjust thresholds.')
-#        else:
-#            for row in secondary:
-#                #sort secondary pairs into groups by threshold distance and year   
-#                if last_row is not None and abs(row[2]) > ter_base:
-#                    if abs(int(row[1][0:4]) - int(last_row[1][0:4])) > ter_year:
-#                        tertiary.append(row) 
-#        groups = []
-#        groups.append(primary,secondary,tertiary)
-#        
 #    #ALL------------------------------------------
 #    else:
 #        for row in data:
